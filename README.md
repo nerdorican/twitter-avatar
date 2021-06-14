ba498e25272f627a005fbaaf10fea88b50968983
nerdorican <jrivera7495@gmail.com>
2021-06-14 00:46:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+  <img src="https://cdn.dribbble.com/users/31506/screenshots/2621777/automatingtweets.png" height="200px"/>
+  <br><br>
+  <b>Get twitter avatar for a username</b>
+  <br><br>
+  Artwork by <a href="https://dribbble.com/Greg_Grigoriou">Greg Grigoriou</a>
+</p>
+
+&nbsp;
+
+#### why?
+
+Twitter deprecated the public avatar API and now it's too much work to grab something that's publically visible ¯\\_(ツ)_/¯
+
+#### usage
+
+Deploy this repo to [vercel](https://vercel.com/). 
+
+URL `https://your-deployment.vercel.app/{username}` returns a png
+
+&nbsp;
+
+#### like it?
+
+:star: this repo
+
+&nbsp;
+
+#### license
+
+MIT © [siddharthkp](https://github.com/siddharthkp)
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cheerio = require("cheerio");
+const request = require("request");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+const cache = {};
+
+const get = (username, size) => {
+  const url = "https://mobile.twitter.com/" + username;
+  return new Promise((resolve) => {
+    if (cache[username]) resolve(cache[username]);
+    else
+      request(url, (_, __, body) => {
+        const $ = cheerio.load(body);
+        const url = ($(".avatar img").attr("src") || "").replace(
+          "_normal",
+          size
+        );
+        cache[username] = url;
+        resolve(url);
+      });
+  });
+};
+
+app.get("/:user", async (req, res, next) => {
+  const result = await get(req.params.user, "_200x200");
+  if (!result) return res.status(404).send('Not Found');
+  request(result).pipe(res);
+});
+
+app.get("/:user/big", async (req, res, next) => {
+  const result = await get(req.params.user, "_400x400");
+  if (!result) return res.status(404).send('Not Found');
+  request(result).pipe(res);
+});
+
+app.get("/:user/full", async (req, res, next) => {
+  const result = await get(req.params.user, "");
+  if (!result) return res.status(404).send('Not Found');
+  request(result).pipe(res);
+});
+
+app.get("/:user/mini", async (req, res, next) => {
+  const result = await get(req.params.user, "_mini");
+  if (!result) return res.status(404).send('Not Found');
+  request(result).pipe(res);
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
+module.exports = app;
